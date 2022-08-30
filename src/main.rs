e31c93d436d20ea25109f1d5b1a990d1e6cba183
José F. Rives <jose@digio.es>
2022-08-30 23:23:05 +0200

    feat: Add simple support for running tasks

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::path::PathBuf;
-use rhai::{Engine, EvalAltResult};
+use rhai::{Engine, EvalAltResult, Scope};
 use clap::Parser;
 
 mod codevar;
@@ -8,13 +8,17 @@ mod codefile;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    // Change CODE env variable if you want
+    /// OPTIONAL: Change CODE env variable if you want
     #[clap(short, long, value_parser)]
     codevar: Option<String>,
 
-    /// Number of times to greet
+    /// OPTIONAL: Script file for execute it
     #[clap(short, long, value_parser)]
     scriptfile: Option<PathBuf>,
+
+    /// OPTIONAL: Runs a specific task declared instead runs the code only
+    #[clap(value_parser)]
+    task: Option<String>,
 }
 
 const ENVCODE: &str = "CODE";
@@ -40,10 +44,34 @@ pub fn main() -> Result<(), Box<EvalAltResult>>
     // Try to load the code from the env variable
     if let Some(code) = codevar::get_code_from_env(codevar){
         // Execute the script
-        engine.run(code.as_str())?;
+        let ast = engine.compile(code.as_str())?;
+
+        match args.task.as_deref(){
+            Some(task_name) => {
+                // Execute function by name
+                let mut scope = Scope::new();
+                engine.call_fn_raw(&mut scope, &ast, true, true, task_name, None, [])?;
+            },
+            None => {
+                // Runs the code without task
+                engine.eval_ast(&ast)?;
+            }
+        }
     }else if let Some(path_code) = codefile::get_code_from_file(scriptfile){
         // Try load the code of the script from a file
-        engine.run_file(path_code)?;
+        let ast = engine.compile_file(path_code)?;
+
+        match args.task.as_deref(){
+            Some(task_name) => {
+                // Execute function by name
+                let mut scope = Scope::new();
+                engine.call_fn_raw(&mut scope, &ast, true, true, task_name, None, [])?;
+            },
+            None => {
+                // Runs the code without task
+                engine.eval_ast(&ast)?;
+            }
+        }
     }
 
     Ok(())
